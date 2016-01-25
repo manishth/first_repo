6cf050293fe891a282435ae8a19b6b5f7c8a299f
manishth <mthakur970@gmail.com>
2016-01-25 15:15:56 +0530

    python code and inheritance code

---
+++ python/inheritance.py
@@ -0,0 +1,16 @@
+class Customer(object):
+    #parent class
+    def __init__(self, customer_id):
+        self.customer_id = customer_id
+
+    def display_cart(self):
+        print "I'm a string that stands in for the contents of your shopping cart!"
+
+class ReturningCustomer(Customer):
+    #notice this class inherits Customer class as an argument
+    def display_order_history(self):
+        print "I'm a string that stands in for your order history!"
+
+monty_python = ReturningCustomer("ID: 12345")
+monty_python.display_cart()
+monty_python.display_order_history()
